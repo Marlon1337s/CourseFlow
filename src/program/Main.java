7734649be283c9737ca7f29ca4885f170b40a1e0
Renata Marinho <renatareismarinho@gmail.com>
2023-11-13 19:35:52 -0300

    Add files via upload

--- src/program/Main.java
+++ src/program/Main.java
@@ -1,14 +1,28 @@
 package program;
-import javax.swing.SwingUtilities;
 
-import forms.gui.FrmLogin;
+import java.sql.SQLException;
+
+//import java.sql.Date;
+//import java.time.LocalDate;
+
+import entidades.Turma;
 
 public class Main {
 
 	public static void main(String[] args) {
-		SwingUtilities.invokeLater(() -> {
-			FrmLogin frmLogin = new FrmLogin();
-            frmLogin.setVisible(true);
-        });
-	}
+		
+		Turma turma = new Turma();
+		
+		turma.setCodTurma(3);
+			
+		
+		try {
+			turma.turmaConsulta();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+		
+		System.out.println(turma.toString());
+ }
 }
